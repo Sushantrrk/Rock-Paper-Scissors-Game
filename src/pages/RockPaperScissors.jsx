ca3347234bc8bea2c064feb81a040295a67533c1
Sushantrrk <sushantrrk@gmail.com>
2025-05-18 01:43:58 +0530

    Updated some css

--- src/pages/RockPaperScissors.jsx
+++ src/pages/RockPaperScissors.jsx
@@ -9,47 +9,47 @@ import worried from "./worried.png";
 import sad from "./sad.png";
 import celebrate from "./celebrate.png";
 import { FaRegHandPaper, FaRegHandRock, FaRegHandScissors } from "react-icons/fa";
-import { s } from "framer-motion/client";
-
-const robotEmotions = {
-    "Waiting": [{ emote: normal, text: "Hmmmmm..." }],
-    "It's a draw!": [{ emote: normal, text: "Hmm... It's a draw!" }],
-    "Computer wins!": [{ emote: celebrate, text: "Yay! I won!" }, { emote: normal, text: "Yay! I won!, One more time?" }],
-    "You win!": [{ emote: cry, text: "Oh no! You won!" }, { emote: angry, text: "Come on! One more time!" }, { emote: sad, text: "Ahhhhhhh! One more time!!!!" }, { emote: worried, text: "Let's try again!" }],
-}
-
-const choices = [
-    { name: "Rock", icon: <FaRegHandRock /> },
-    { name: "Paper", icon: <FaRegHandPaper /> },
-    { name: "Scissors", icon: <FaRegHandScissors /> },
-];
-
-const getResult = (player, computer) => {
-    if (player === computer) return "It's a draw!";
-    if (
-        (player === "Rock" && computer === "Scissors") ||
-        (player === "Paper" && computer === "Rock") ||
-        (player === "Scissors" && computer === "Paper")
-    ) {
-        return "You win!";
-    }
-    return "Computer wins!";
-};
 
 export default function RockPaperScissors() {
-    document.title = "Rock Paper Scissors";
-    const handSigns = [<FaRegHandRock />, <FaRegHandPaper style={{ rotate: '180deg' }} />, <FaRegHandScissors />];
+
+    const handSigns = [<FaRegHandRock />, <FaRegHandPaper />, <FaRegHandScissors />];
     const [playerChoice, setPlayerChoice] = useState(null);
     const [computerChoice, setComputerChoice] = useState(null);
     const [result, setResult] = useState("");
     const [robotAction, setRobotAction] = useState(normal);
     const [robotText, setRobotText] = useState("Hi There! I am your robot friend. Let's play Rock Paper Scissors!");
-    const [shuffle, setshuffle] = useState(false);
     const [shuffleIcon, setshuffleIcon] = useState(null);
+    const [gameStart, setGameStart] = useState(false);
+    const [gameStartText, setGameStartText] = useState("");
+
+    const robotEmotions = {
+        "Waiting": [{ emote: normal, text: "Hmmmmm..." }, { emote: normal, text: "Let me show you how it's done!" }, { emote: normal, text: "I will beat you!" }],
+        "It's a draw!": [{ emote: normal, text: "Hmm... It's a draw!" }, { emote: normal, text: "Let's try again!" }],
+        "Computer wins!": [{ emote: celebrate, text: "Yay! I won!" }, { emote: normal, text: "Yay! I won!, One more time?" }],
+        "You win!": [{ emote: cry, text: "Oh no! You won!" }, { emote: cry, text: "This doesn't count! One more time!" }, { emote: angry, text: "Come on! One more time!" }, { emote: sad, text: "Shoot! One more time!!!!" }, { emote: worried, text: "Let's try again!" }],
+    }
+
+    const choices = [
+        { name: "Rock", icon: <FaRegHandRock /> },
+        { name: "Paper", icon: <FaRegHandPaper /> },
+        { name: "Scissors", icon: <FaRegHandScissors /> },
+    ];
+
+    const getResult = (player, computer) => {
+        if (player === computer) return "It's a draw!";
+        if (
+            (player === "Rock" && computer === "Scissors") ||
+            (player === "Paper" && computer === "Rock") ||
+            (player === "Scissors" && computer === "Paper")
+        ) {
+            return "You win!";
+        }
+        return "Computer wins!";
+    };
 
     useEffect(() => {
         if (result === "Start") {
-            setshuffle(true);
+            setGameStart(true);
             let randomIndex = Math.floor(Math.random() * robotEmotions["Waiting"].length);
             const { emote, text } = robotEmotions["Waiting"][randomIndex];
             setRobotAction(emote);
@@ -76,21 +76,31 @@ export default function RockPaperScissors() {
     }, [result])
 
     const handleChoice = (choice) => {
-        if (shuffle) return;
+        if (gameStart) return;
         setResult("Start");
         setPlayerChoice(choice);
-        let timeoutinterval = setTimeout(() => {
+        setTimeout(() => {
+            setGameStartText("Rock");
+        }, 0);
+        setTimeout(() => {
+            setGameStartText("Paper");
+        }, 1000);
+        setTimeout(() => {
+            setGameStartText("Scissors");
+        }, 2000);
+        setTimeout(() => {
+            setGameStartText("Shoot!");
+        }, 3000);
+
+        setTimeout(() => {
             const randomIndex = Math.floor(Math.random() * choices.length);
             const computer = choices[randomIndex];
             setComputerChoice(computer);
             const gameResult = getResult(choice.name, computer.name);
-            setshuffle(false);
+            setGameStart(false);
             setResult(gameResult);
-        }, 2000);
+        }, 3000);
 
-        return () => {
-            clearTimeout(timeoutinterval);
-        };
     };
 
     useEffect(() => {
@@ -123,23 +133,27 @@ export default function RockPaperScissors() {
                     </div>
                 </div>
                 <div className="outcome-computer">
-                    {computerChoice && !shuffle ? computerChoice.icon : (
+                    {computerChoice && !gameStart ? computerChoice.icon : (
                         shuffleIcon
                     )}
                 </div>
                 <div style={{ height: '100%', width: ".1vw", backgroundColor: '#e0f2f7' }}></div>
                 <div className="outcome-player">
-                    {playerChoice ? playerChoice.icon : (
+                    {playerChoice && !gameStart ? playerChoice.icon : (
                         shuffleIcon
                     )}
                 </div>
                 <div className="playerChoice">
+                    Choose
                     {choices.map((choice) => (
                         <div className="choices-icons" key={choice.name} onClick={() => handleChoice(choice)}>
                             {choice.icon}
                         </div>
                     ))}
                 </div>
+            </div>
+            <div className="big-text">
+                {gameStartText}
             </div>
         </div>
     );
